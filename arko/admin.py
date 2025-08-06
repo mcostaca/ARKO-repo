e8fcfd6290ab98dc9b2c194fc93dbbdb833ba650
Matheus <matheus.alves@uncisal.edu.br>
2025-08-06 00:47:44 -0300

    feat: register models on admin

--- arko/admin.py
+++ arko/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
-
+from .models import Regiao, Estado, Mesoregiao, Microregiao, RegiaoIntermediaria, RegiaoImediata, Municipio, Distrito
 # Register your models here.
+
+
+
+@admin.register(Regiao)
+class RegiaoAdmin(admin.ModelAdmin):
+    list_display = ('id_ibge', 'sigla', 'nome')
+    search_fields = ('nome', 'sigla', 'id_ibge')
+
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('id_ibge', 'sigla', 'nome', 'regiao')
+    search_fields = ('nome', 'sigla', 'id_ibge')
+    list_filter = ('regiao',)
+
+@admin.register(Mesoregiao)
+class MesoregiaoAdmin(admin.ModelAdmin):
+    list_display = ('id_ibge', 'nome', 'estado')
+    search_fields = ('nome', 'id_ibge')
+    list_filter = ('estado',)
+
+@admin.register(Microregiao)
+class MicroregiaoAdmin(admin.ModelAdmin):
+    list_display = ('id_ibge', 'nome', 'mesoregiao')
+    search_fields = ('nome', 'id_ibge')
+    list_filter = ('mesoregiao',)
+
+@admin.register(RegiaoIntermediaria)
+class RegiaoIntermediariaAdmin(admin.ModelAdmin):
+    list_display = ('id_ibge', 'nome', 'estado')
+    search_fields = ('nome', 'id_ibge')
+    list_filter = ('estado',)
+
+@admin.register(RegiaoImediata)
+class RegiaoImediataAdmin(admin.ModelAdmin):
+    list_display = ('id_ibge', 'nome', 'regiao_intermediaria')
+    search_fields = ('nome', 'id_ibge')
+    list_filter = ('regiao_intermediaria',)
+
+@admin.register(Municipio)
+class MunicipioAdmin(admin.ModelAdmin):
+    list_display = ('id_ibge', 'nome', 'microrregiao', 'mesoregiao', 'estado', 'regiao_imediata', 'regiao_intermediaria')
+    search_fields = ('nome', 'id_ibge')
+    list_filter = ('estado', 'mesoregiao', 'microrregiao', 'regiao_imediata', 'regiao_intermediaria')
+
+@admin.register(Distrito)
+class DistritoAdmin(admin.ModelAdmin):
+    list_display = ('id_ibge', 'nome', 'municipio')
+    search_fields = ('nome', 'id_ibge')
+    autocomplete_fields = ['municipio']
+
